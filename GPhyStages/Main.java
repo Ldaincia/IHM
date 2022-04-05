3099470377679976a31992e2b67cb7fe95fc0562
paulineaudru <972pauline.audru@gmail.com>
2022-04-05 16:14:23 +0200

    On a écrit le début du main wallah

--- GPhyStages/Main.java
+++ GPhyStages/Main.java
@@ -1,6 +1,21 @@
 package GPhyStages;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.application.Platform;
+import javafx.scene.Scene;
 
+public class Main extends Application {
+    public static void main (String []args) {
+        launch(args);
+    }
+    @Override
 
+    public void start (Stage primaryStage) {
+        primaryStage.setTitle(" TITRE FENETRE ");
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
+        primaryStage.setScene(new Scene(new Label(" HELLO WORLD "), 1280, 720));
+        primaryStage.show();
+    }
 }
