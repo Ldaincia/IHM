5bb7a3048e2c0b7405f45a7c516ea2e23105727a
Arnaud Livertout <arnaud.livertout@laposte.net>
2022-04-12 18:47:01 +0200

    fxml

--- GPhyStages/Main.java
+++ GPhyStages/Main.java
@@ -1,74 +1,21 @@
 package GPhyStages;
 
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-
-
-import Modules.Accueil.AccueilController;
-import Modules.Stages.StageController;
-import Modules.NosInfos.InfosController;
-import Modules.Stages.AjoutStage.NouveauStageController;
 import javafx.application.Application;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import javafx.application.Platform;
-
+import javafx.scene.Scene;
 
 public class Main extends Application {
-
-    private AccueilController accueilController;
-    private StageController stageController;
-    private InfosController infosController;
-    private NouveauStageController nouveauStageController;
-    public Tools tools;
-
-    @Override
-    public void init() {
-        accueilController = new AccueilController(this);
-        stageController = new StageController(this);
-        infosController = new InfosController(this);
-        nouveauStageController = new NouveauStageController(this);
-
-
-
+    public static void main (String []args) {
+        launch(args);
     }
     @Override
-    public void start(Stage primaryStage) {
+
+    public void start (Stage primaryStage) {
         primaryStage.setTitle(" TITRE FENETRE ");
         primaryStage.setOnCloseRequest(e -> Platform.exit());
         primaryStage.setScene(new Scene(new Label(" HELLO WORLD "), 1280, 720));
         primaryStage.show();
     }
-
-
-
-
-
-    // TOUS NOS GETTERS
-    public AccueilController getAccueilController() {
-        return accueilController;
-    }
-
-
-    public StageController getStageController() {
-        return stageController;
-    }
-
-
-    public NouveauStageController getNouveauStageController() {
-        return nouveauStageController;
-    }
-
-
-    public InfosController getInfosController() {
-        return infosController;
-    }
-
-
-
-
 }
-
-
-
-
